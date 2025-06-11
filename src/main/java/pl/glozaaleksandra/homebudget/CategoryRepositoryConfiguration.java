040864dda2784063450412a634f912fdacd1a265
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-06-11 20:33:02 +0200

    (fix) comment @Bean to avoid 2 Beans

--- src/main/java/pl/glozaaleksandra/homebudget/CategoryRepositoryConfiguration.java
+++ src/main/java/pl/glozaaleksandra/homebudget/CategoryRepositoryConfiguration.java
@@ -1,6 +1,5 @@
 package pl.glozaaleksandra.homebudget;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.glozaaleksandra.homebudget.category.CategoryRepository;
 import pl.glozaaleksandra.homebudget.category.ListBasedCategoryRepository;
@@ -8,7 +7,7 @@ import pl.glozaaleksandra.homebudget.category.ListBasedCategoryRepository;
 @Configuration
 public class CategoryRepositoryConfiguration {
 
-  @Bean
+//  @Bean
   public CategoryRepository listBasedCategoryRepository(){
     return new ListBasedCategoryRepository();
   }
