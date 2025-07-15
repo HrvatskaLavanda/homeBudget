88dc8071528a256a0e46712f875ac586d48599e9
PaniOdProgramowania <pani.od.programowania@gmail.com>
2025-07-15 17:06:10 +0200

    Lesson 15.07 - add tests for expense service, add homework

---
+++ src/test/java/pl/glozaaleksandra/homebudget/service/expenses/ExpenseServiceTest.java
@@ -0,0 +1,72 @@
+package pl.glozaaleksandra.homebudget.service.expenses;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.time.Instant;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import pl.glozaaleksandra.homebudget.Person;
+import pl.glozaaleksandra.homebudget.repository.expense.Expense;
+import pl.glozaaleksandra.homebudget.repository.expense.ExpenseRepository;
+import pl.glozaaleksandra.homebudget.repository.product.Product;
+import pl.glozaaleksandra.homebudget.service.person.PersonService;
+
+class ExpenseServiceTest {
+
+  @Mock
+  private ExpenseRepository expenseRepository;
+
+  @Mock
+  private PersonService personService;
+
+  @Mock
+  private Product product1;
+
+  @Mock
+  private Product product2;
+
+  @Mock
+  private Person person;
+
+  @Mock
+  private Expense expense;
+
+  private ExpenseService expenseService;
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+    expenseService = new ExpenseService(expenseRepository, personService);
+  }
+
+  @Test
+  public void shouldAddNewExpense(){
+    // given
+    String buyerName = "Tomek";
+    List<Product> products = List.of(product1, product2);
+    Instant purchaseTime = Instant.now();
+    when(personService.personExistsByName(eq(buyerName))).thenReturn(true);
+    when(personService.getByName(eq(buyerName))).thenReturn(person);
+    when(expenseRepository.save(any())).thenReturn(expense);
+    // todo - fill in when(expense....)
+
+    // when
+    Expense expected = expenseService.addNewExpense(buyerName, products, purchaseTime);
+
+    // then
+    Assertions.assertEquals(purchaseTime, expected.getExpenseDatetime());
+    Assertions.assertEquals(person, expected.getBuyer());
+    Assertions.assertEquals(products, expected.getProducts());
+  }
+
+  // todo 2nd test for else
+
+  // todo - tests for arguments (what if buyerName is null / empty? what if products null / empty? what if purchaseTime from
+  //  future or null?
+}
