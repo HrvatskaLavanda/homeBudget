08f23495ef3217dede6d613b547063ca375e18ff
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-07-23 16:46:12 +0200

    (fix) correct code for addNewExpense() method when parameters are null

--- src/main/java/pl/glozaaleksandra/homebudget/service/expenses/ExpenseService.java
+++ src/main/java/pl/glozaaleksandra/homebudget/service/expenses/ExpenseService.java
@@ -1,7 +1,5 @@
 package pl.glozaaleksandra.homebudget.service.expenses;
 
-import java.time.Instant;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import pl.glozaaleksandra.homebudget.Person;
 import pl.glozaaleksandra.homebudget.repository.expense.Expense;
@@ -9,23 +7,32 @@ import pl.glozaaleksandra.homebudget.repository.expense.ExpenseRepository;
 import pl.glozaaleksandra.homebudget.repository.product.Product;
 import pl.glozaaleksandra.homebudget.service.person.PersonService;
 
+import java.time.Instant;
+import java.util.List;
+
 @AllArgsConstructor
 public class ExpenseService {
-  private ExpenseRepository expenseRepository;
-  private PersonService personService;
+    private ExpenseRepository expenseRepository;
+    private PersonService personService;
 
-  public Expense addNewExpense(String buyerName, List<Product> boughtProducts, Instant purchaseTime){
-    Person person;
-    if (personService.personExistsByName(buyerName)){
-      person = personService.getByName(buyerName);
-    } else {
-      person = personService.saveNewPerson(buyerName);
+    public Expense addNewExpense(String buyerName, List<Product> boughtProducts, Instant purchaseTime) {
+        Person person;
+        if (buyerName == null) {
+            throw new IllegalArgumentException("Buyer name is null.");
+        }
+        if (boughtProducts == null || boughtProducts.isEmpty()) {
+            throw new IllegalArgumentException("Buyer name is null.");
+        }
+        if (personService.personExistsByName(buyerName)) {
+            person = personService.getByName(buyerName);
+        } else {
+            person = personService.saveNewPerson(buyerName);
+        }
+        Expense expense = Expense.builder()
+                .expenseDatetime(purchaseTime)
+                .buyer(person)
+                .products(boughtProducts)
+                .build();
+        return expenseRepository.save(expense);
     }
-    Expense expense = Expense.builder()
-        .expenseDatetime(purchaseTime)
-        .buyer(person)
-        .products(boughtProducts)
-        .build();
-    return expenseRepository.save(expense);
-  }
 }
