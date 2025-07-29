7c16f638074081a515b4e94115c531ccce5dcafe
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-07-29 16:45:08 +0200

    (feature) add test for findAll() method

--- src/test/java/pl/glozaaleksandra/homebudget/repository/product/ListBasedProductRepositoryTest.java
+++ src/test/java/pl/glozaaleksandra/homebudget/repository/product/ListBasedProductRepositoryTest.java
@@ -2,16 +2,21 @@ package pl.glozaaleksandra.homebudget.repository.product;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import pl.glozaaleksandra.homebudget.repository.category.Category;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 class ListBasedProductRepositoryTest {
+    @Mock
+    ListBasedProductRepository repository = new ListBasedProductRepository();
 
     @Test
     public void shouldAddNewProduct() {
         //given
-        ListBasedProductRepository repository = new ListBasedProductRepository();
+
         Category fruitsCategory = new Category("Fruits");
         Product expectedProduct = new Product(BigDecimal.valueOf(100), "apple", fruitsCategory);
 
@@ -20,6 +25,19 @@ class ListBasedProductRepositoryTest {
 
         //then
         Assertions.assertEquals(actualProduct, expectedProduct);
+    }
+
+    @Test
+    public void shouldFindAllProducts() {
+        //given
+        List<Product> expectedAllProducts = new ArrayList<>();
+
+        //when
+        List<Product> actualAllProducts = repository.findAll();
+
+        //then
+        Assertions.assertEquals(actualAllProducts, expectedAllProducts);
+
     }
 
 
