c49d9260cda5853ee8c27e0816c0db21f961fcd5
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-08-30 12:24:16 +0200

    (fix) change imports for repositories

--- src/main/java/pl/glozaaleksandra/homebudget/HomebudgetApplication.java
+++ src/main/java/pl/glozaaleksandra/homebudget/HomebudgetApplication.java
@@ -3,16 +3,12 @@ package pl.glozaaleksandra.homebudget;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import pl.glozaaleksandra.homebudget.model.Category;
-import pl.glozaaleksandra.homebudget.model.Product;
-import pl.glozaaleksandra.homebudget.nodatabase.repository.category.CategoryRepository;
-import pl.glozaaleksandra.homebudget.nodatabase.repository.category.ListBasedCategoryRepository;
-import pl.glozaaleksandra.homebudget.nodatabase.repository.product.ListBasedProductRepository;
-import pl.glozaaleksandra.homebudget.nodatabase.repository.product.ProductRepository;
+import pl.glozaaleksandra.homebudget.entities.CategoryEntity;
+import pl.glozaaleksandra.homebudget.entities.ProductEntity;
+import pl.glozaaleksandra.homebudget.repository.CategoryRepository;
+import pl.glozaaleksandra.homebudget.repository.ProductRepository;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
 public class HomebudgetApplication {
@@ -20,70 +16,70 @@ public class HomebudgetApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(HomebudgetApplication.class);
         CategoryRepository categoryRepository = context.getBean(CategoryRepository.class);
-        List<Category> categories = categoryRepository.findAll();
+        List<CategoryEntity> categories = categoryRepository.findAll();
         System.out.println(categories);
 
         ProductRepository productRepository = context.getBean(ProductRepository.class);
-        List<Product> products = productRepository.findAll();
+        List<ProductEntity> products = productRepository.findAll();
         System.out.println(products);
 
     }
 
-    private static void version1() {
-        CategoryRepository categoryRepository = new ListBasedCategoryRepository();
-        ProductRepository productRepository = new ListBasedProductRepository();
-
-        var foodCategory = new Category("food");
-        var cleaningCategory = new Category("cleaning");
-        var developmentCategory = new Category("development");
-        var entertainmentCategory = new Category("entertainment");
-
-        var cinema = new Product(BigDecimal.valueOf(100), "cinema", entertainmentCategory);
-        var tonic = new Product(BigDecimal.valueOf(9), "tonic", foodCategory);
-        var tuna = new Product(BigDecimal.valueOf(5), "tuna", foodCategory);
-
-        categoryRepository.save(foodCategory);
-        categoryRepository.save(cleaningCategory);
-        categoryRepository.save(developmentCategory);
-        categoryRepository.save(entertainmentCategory);
-
-        categoryRepository.delete("food");
-
-        List<Category> categories = categoryRepository.findAll();
-
-        categoryRepository.update(entertainmentCategory, "bread");
-
-        Optional<Category> foundCategory = categoryRepository.findByName("bread");
-
-        for (Category c : categories) {
-            System.out.println(c.getName());
-        }
-
-        System.out.println("Found category: " + foundCategory);
-
-        System.out.println("------------------------");
-        System.out.println("Product section: ");
-
-        productRepository.save(cinema);
-        productRepository.save(tonic);
-        productRepository.save(tuna);
-
-        List<Product> allProducts = productRepository.findAll();
-        System.out.println("All products: " + allProducts);
-
-        Optional<Product> foundProduct = productRepository.findByName("cinema");
-        System.out.println("Found product: " + foundProduct);
-
-        productRepository.update(cinema, "theater");
-
-        Optional<Product> foundProduct1 = productRepository.findByName("theater");
-        System.out.println("Found updated product: " + foundProduct1);
-
-        productRepository.delete("theater");
-
-        allProducts = productRepository.findAll();
-        System.out.println("All products: " + allProducts);
-    }
+//    private static void version1() {
+//        CategoryRepository categoryRepository = new ListBasedCategoryRepository();
+//        ProductRepository productRepository = new ListBasedProductRepository();
+//
+//        var foodCategory = new Category("food");
+//        var cleaningCategory = new Category("cleaning");
+//        var developmentCategory = new Category("development");
+//        var entertainmentCategory = new Category("entertainment");
+//
+//        var cinema = new Product(BigDecimal.valueOf(100), "cinema", entertainmentCategory);
+//        var tonic = new Product(BigDecimal.valueOf(9), "tonic", foodCategory);
+//        var tuna = new Product(BigDecimal.valueOf(5), "tuna", foodCategory);
+//
+//        categoryRepository.save(foodCategory);
+//        categoryRepository.save(cleaningCategory);
+//        categoryRepository.save(developmentCategory);
+//        categoryRepository.save(entertainmentCategory);
+//
+//        categoryRepository.delete("food");
+//
+//        List<Category> categories = categoryRepository.findAll();
+//
+//        categoryRepository.update(entertainmentCategory, "bread");
+//
+//        Optional<Category> foundCategory = categoryRepository.findByName("bread");
+//
+//        for (Category c : categories) {
+//            System.out.println(c.getName());
+//        }
+//
+//        System.out.println("Found category: " + foundCategory);
+//
+//        System.out.println("------------------------");
+//        System.out.println("Product section: ");
+//
+//        productRepository.save(cinema);
+//        productRepository.save(tonic);
+//        productRepository.save(tuna);
+//
+//        List<Product> allProducts = productRepository.findAll();
+//        System.out.println("All products: " + allProducts);
+//
+//        Optional<Product> foundProduct = productRepository.findByName("cinema");
+//        System.out.println("Found product: " + foundProduct);
+//
+//        productRepository.update(cinema, "theater");
+//
+//        Optional<Product> foundProduct1 = productRepository.findByName("theater");
+//        System.out.println("Found updated product: " + foundProduct1);
+//
+//        productRepository.delete("theater");
+//
+//        allProducts = productRepository.findAll();
+//        System.out.println("All products: " + allProducts);
+//    }
 
 
 }
