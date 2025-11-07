24b0861c895c9a4ba35ddc4539b49920ac31d370
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-11-07 21:15:47 +0100

    (feat) replace each CategoryEntity with Category using Stream

--- src/main/java/pl/glozaaleksandra/homebudget/controllers/category/CategoryControllery.java
+++ src/main/java/pl/glozaaleksandra/homebudget/controllers/category/CategoryControllery.java
@@ -3,21 +3,27 @@ package pl.glozaaleksandra.homebudget.controllers.category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.glozaaleksandra.homebudget.entities.CategoryEntity;
 import pl.glozaaleksandra.homebudget.model.Category;
 import pl.glozaaleksandra.homebudget.service.category.CategoryService;
+import pl.glozaaleksandra.serializer.CategorySerializer;
 
 import java.util.List;
 
 @RestController
 public class CategoryControllery {
-  private CategoryService categoryService;
+    private CategoryService categoryService;
 
-  public CategoryControllery(@Autowired CategoryService categoryService) {
-    this.categoryService = categoryService;
-  }
+    public CategoryControllery(@Autowired CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
-  @GetMapping("/categories")
-  public List<Category> getAll() {
-    return categoryService.findAll();
-  }
+    @GetMapping("/categories")
+    public List<Category> getAll() {
+        List<CategoryEntity> categoryEntities = categoryService.findAll();
+        List<Category> categories = categoryEntities.stream()
+                .map(categoryEntity -> CategorySerializer.fromEntity(categoryEntity))
+                .toList();
+        return categories;
+    }
 }
